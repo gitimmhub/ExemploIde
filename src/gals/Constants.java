7a46cfd4fa4753b8aa0d80dbf36fb0e7eb4c5736
Matheus <matheus-tb@edu.univali.br>
2025-05-06 19:36:23 -0300

    first commit

---
+++ src/gals/Constants.java
@@ -0,0 +1,64 @@
+public interface Constants extends ScannerConstants, ParserConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_COMENTARIO = 2;
+    int t_COMENTARIO_BLOCO = 3;
+    int t_INTEIRO = 4;
+    int t_DECIMAL = 5;
+    int t_CARACTERE = 6;
+    int t_TEXTO = 7;
+    int t_ENTRADA = 8;
+    int t_SAIDA = 9;
+    int t_RETORNO = 10;
+    int t_ENTAO = 11;
+    int t_SE = 12;
+    int t_SENAO = 13;
+    int t_ENQUANTO = 14;
+    int t_FACA = 15;
+    int t_PARA = 16;
+    int t_CONSTANTE = 17;
+    int t_VARIAVEL = 18;
+    int t_FUNCAO = 19;
+    int t_PONTO = 20;
+    int t_PONTO_VIRGULA = 21;
+    int t_DOIS_PONTOS = 22;
+    int t_COLCHETE_ABRE = 23;
+    int t_COLCHETE_FECHA = 24;
+    int t_PARENTESES_ABRE = 25;
+    int t_PARENTESES_FECHA = 26;
+    int t_CHAVES_ABRE = 27;
+    int t_CHAVES_FECHA = 28;
+    int t_VIRGULA = 29;
+    int t_RECEBE = 30;
+    int t_EXPOENTE = 31;
+    int t_SOMA = 32;
+    int t_SUBTRACAO = 33;
+    int t_MULTIPLICACAO = 34;
+    int t_DIVISAO = 35;
+    int t_RESTO = 36;
+    int t_IGUAL = 37;
+    int t_MAIOR = 38;
+    int t_MENOR = 39;
+    int t_MAIOR_IGUAL = 40;
+    int t_MENOR_IGUAL = 41;
+    int t_DIFERENTE = 42;
+    int t_E = 43;
+    int t_OU = 44;
+    int t_NAO = 45;
+    int t_DESLOCAMENTO_DIREITA = 46;
+    int t_DESLOCAMENTO_ESQUERDA = 47;
+    int t_E_BAB = 48;
+    int t_OU_BAB = 49;
+    int t_OU_EXCLUSIVO = 50;
+    int t_COMPLEMENTO = 51;
+    int t_ID = 52;
+    int t_CHAR = 53;
+    int t_INT = 54;
+    int t_FLOAT = 55;
+    int t_STRING = 56;
+    int t_VOID = 57;
+    int t_BOOLEAN = 58;
+
+}
