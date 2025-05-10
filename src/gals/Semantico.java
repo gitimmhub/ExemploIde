fd562d1ca59c2fd8185ef63db50cc5f00cdbc04b
Matheus <matheus-tb@edu.univali.br>
2025-05-10 16:34:33 -0300

    gals with #n

--- src/gals/Semantico.java
+++ src/gals/Semantico.java
@@ -1,8 +1,16 @@
 package gals;
 public class Semantico implements Constants
 {
-    public void executeAction(int action, Token token)	throws SemanticError
-    {
-        System.out.println("Ação #"+action+", Token: "+token);
-    }	
+    public void executeAction(int action, Token token) throws SemanticError {
+        System.out.println("Ação semântica: #" + action + " - " + token.getLexeme());
+        switch (action) {
+            case 1:
+                
+                break;
+
+            default:
+                throw new SemanticError("Ação semântica não definida: #" + action);
+        }
+    }
 }
+                // Ação semântica correspondente
