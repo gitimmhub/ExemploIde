30dc9f1e092ac8198317fd5ef8357f9d0f3d106b
Matheus <matheus-tb@edu.univali.br>
2025-06-01 22:58:59 -0300

    vetor arrumado baby!

--- src/gals/Semantico.java
+++ src/gals/Semantico.java
@@ -9,6 +9,7 @@ public class Semantico implements Constants {
     private Map<String, Simbolo> symbolTable = new HashMap<>();
     private String tipoAtual = null;
     private boolean isVetor = false;
+    private int tamanhoVetor = 0;
     private Integer escopoAtual = 0;
     private String idAtual = null;
     private int escopoMax = 0;
@@ -96,18 +97,26 @@ public class Semantico implements Constants {
                 isVetor = false;
                     System.err.println(symbolTable);
                 break;
-
             case 3:
-                tipoAtual = token.getLexeme();
+                tipoAtual = token.getLexeme();  // Armazena o tipo base
+                isVetor = false;                // Inicialmente, assume que não é vetor
                 break;
 
             case 4:
-                isVetor = true;
+                tipoAtual += "[]";             // Marca como tipo vetor (ex: int[])
+                isVetor = true;                // Marca a flag global
                 break;
 
             case 5:
+            /* 
+                try {
+                    tamanhoAtual = Integer.parseInt(token.getLexeme());  // ex: "10" → 10
+                } catch (NumberFormatException e) {
+                    System.err.println("Erro: tamanho do vetor inválido: " + token.getLexeme());
+                    tamanhoAtual = -1;
+                }
                 break;
-
+            */
             case 6:
                 break;
 
